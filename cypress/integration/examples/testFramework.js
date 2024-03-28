d9b1f56db0c62a54253d2444e470ca9e1f13724a
luciana lopez <lucianalopeztandil625@gmail.com>
2024-03-28 02:27:39 -0300

    TestFramework updated

--- cypress/integration/examples/testFramework.js
+++ cypress/integration/examples/testFramework.js
@@ -1,12 +1,20 @@
+/// <reference types="Cypress"/>
+
 describe('Testing angular page', function(){
 
+    before(function(){
+        cy.fixture('example.json').then((data)=>{
+            this.data = data;
+        }
+    )})
 
     it('Complete operation', function(){
         cy.visit('https://rahulshettyacademy.com/angularpractice/shop');
+        cy.log('The data is: ' + this.data.products);
         cy.get('app-card-list app-card').as('products').each(($element, index, $list)=>{
             const nameElement = $element.find('.card-title a').text();
             cy.log('The name of the product is: ' + nameElement);
-            if(nameElement == 'Blackberry'){
+              if(this.data.products.includes(nameElement)){
                 cy.log('The element was found: ' + nameElement);
                 cy.get('@products').eq(index).find('.btn').click();
             }
